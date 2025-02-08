961b1ca1f073a44f387d61b276d51317acde11e2
Guillermo <ramireziiiguillermo@gmail.com>
2025-02-07 21:50:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,24 @@ const knex = require('knex');
 
 const app = express();
 
-let initalPath = path.join()
+let initalPath = path.join(__dirname, "public")
+
+app.use(bodyParser.json());
+app.use(express.static(initalPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initalPath, "index.html"));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(initalPath, "login.html"));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(initalPath, "register.html"));
+})
+
+
+app.listen(3000, (req, res) => {
+    console.log('listening on port 3000......')
+})
